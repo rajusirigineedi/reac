2cf2b7c8c2ec5bcb984819ea88049d971c8af1f6
Raju Sirigineedi <75467803+rajusirigineedi@users.noreply.github.com>
2022-09-12 12:11:33 +0530

    😂 check is runnign

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>This is awesomw</h1>;
+      <h1>This is awesome</h1>;
       <hr />
       <MarketingApp />
     </>
