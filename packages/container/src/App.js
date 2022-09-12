7e309afb925879428a64eca8f0cdadacdfd65cb7
Raju Sirigineedi <75467803+rajusirigineedi@users.noreply.github.com>
2022-09-12 11:21:20 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <>
+      <h1>This is awesomw</h1>;
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
