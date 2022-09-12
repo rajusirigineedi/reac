4a6e91f4d0e4cf9ea92e239a71cb7b98a30641be
Raju Sirigineedi <75467803+rajusirigineedi@users.noreply.github.com>
2022-09-12 15:54:19 +0530

    🙃 add invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <>
-      <h1>This is an awesome</h1>;
+      <h1>This is an awesome app</h1>;
       <hr />
       <MarketingApp />
     </>
