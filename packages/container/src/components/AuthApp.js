fe2df823f6aeb0b6f922021bf9df237265d21624
Raju Sirigineedi <75467803+rajusirigineedi@users.noreply.github.com>
2022-09-13 12:41:08 +0530

    🔥 Add auth subapp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,14 +1,15 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "auth/AuthApp";
 import { useHistory } from "react-router-dom";
 import React, { useRef, useEffect } from "react";
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
+      onSignIn,
       onNavigate: ({ pathname: nextPathName }) => {
         const { pathname } = history.location;
         if (pathname !== nextPathName) history.push(nextPathName);
